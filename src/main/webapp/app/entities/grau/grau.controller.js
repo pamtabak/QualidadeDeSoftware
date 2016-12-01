606b2be61007960d557988640077f0583f733c0d
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-12-01 16:23:55 -0200

    cleaning the code;

--- src/main/webapp/app/entities/grau/grau.controller.js
+++ src/main/webapp/app/entities/grau/grau.controller.js
@@ -7,13 +7,12 @@
 
   GrauController.$inject = ['$scope', '$state','Principal',  'Grau', 'Disciplina'];
 
-  function GrauController ($scope, $state, Profile,  Inscricao, Disciplina) {
+  function GrauController ($scope, $state, Principal,  Inscricao, Disciplina) {
     var vm = this;
 
     vm.inscricaos = [];
     vm.discplinas = [];
-    console.log(Profile);
-    Profile.identity().then(r => {
+    Principal.identity().then(r => {
       var authorities = r.authorities;
       if(authorities.indexOf("ROLE_ADMIN") != -1 || authorities.indexOf('ROLE_SECRETARIA') != -1) {
         loadAll();
@@ -26,7 +25,6 @@
       }
     });
 
-    console.log(vm);
         function loadAll() {
             Inscricao.query(function(result) {
                 vm.inscricaos = result;
