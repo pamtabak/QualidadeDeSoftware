9ab0e7a30ece9cf07b0f12c518a56559e97a46dc
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-12-01 13:23:18 -0200

    secretaria and ADMIN can see all Incrisao. Professor can see only his disciplina;

--- src/main/webapp/app/entities/grau/grau.controller.js
+++ src/main/webapp/app/entities/grau/grau.controller.js
@@ -5,18 +5,28 @@
         .module('jhipsterApp')
         .controller('GrauController', GrauController);
 
-    GrauController.$inject = ['$scope', '$state', 'Grau'];
+  GrauController.$inject = ['$scope', '$state','Principal',  'Grau', 'Disciplina'];
 
-    function GrauController ($scope, $state, Inscricao, Disciplina) {
-        var vm = this;
-        
-        vm.inscricaos = [];
-        vm.discplinas = [];
+  function GrauController ($scope, $state, Profile,  Inscricao, Disciplina) {
+    var vm = this;
 
-        console.log($scope,'-----',$state)
-
+    vm.inscricaos = [];
+    vm.discplinas = [];
+    console.log(Profile);
+    Profile.identity().then(r => {
+      var authorities = r.authorities;
+      if(authorities.indexOf("ROLE_ADMIN") != -1 || authorities.indexOf('ROLE_SECRETARIA') != -1) {
         loadAll();
-    
+      } else {
+        Inscricao.query(function(result) {
+          vm.inscricaos = result.filter(function(element) {
+            return element.disciplina.professor.login === r.login;
+          });
+        });
+      }
+    });
+
+    console.log(vm);
         function loadAll() {
             Inscricao.query(function(result) {
                 vm.inscricaos = result;
