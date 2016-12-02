7862f51c06fbea0bf8b5930175aa47e33d6d843d
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-12-02 21:49:44 -0200

    handling with visualization permission of Trancamento and Inscricao;

--- src/main/webapp/app/entities/inscricao/inscricao.controller.js
+++ src/main/webapp/app/entities/inscricao/inscricao.controller.js
@@ -5,19 +5,31 @@
         .module('jhipsterApp')
         .controller('InscricaoController', InscricaoController);
 
-    InscricaoController.$inject = ['$scope', '$state', 'Inscricao'];
+  InscricaoController.$inject = ['$scope', '$state', 'Principal', 'Inscricao'];
 
-    function InscricaoController ($scope, $state, Inscricao) {
-        var vm = this;
-        
-        vm.inscricaos = [];
+  function InscricaoController ($scope, $state, Principal, Inscricao) {
+    var vm = this;
 
+    vm.inscricaos = [];
+
+    Principal.identity().then(r => {
+      var authorities = r.authorities;
+      if(authorities.indexOf("ROLE_ADMIN") != -1 || authorities.indexOf('ROLE_SECRETARIA') != -1) {
         loadAll();
+      } else if(authorities.indexOf('ROLE_ALUNO')) {
+        Inscricao.query(function(result) {
+          vm.inscricaos = result.filter(function(element) {
+            return element.aluno.login === r.login;
+          });
+        });
+      }
+    });
 
-        function loadAll() {
-            Inscricao.query(function(result) {
-                vm.inscricaos = result;
-            });
-        }
+
+    function loadAll() {
+      Inscricao.query(function(result) {
+        vm.inscricaos = result;
+      });
     }
+  }
 })();
--- src/main/webapp/app/entities/trancamento/trancamento.controller.js
+++ src/main/webapp/app/entities/trancamento/trancamento.controller.js
@@ -11,15 +11,17 @@
     var vm = this;
 
     vm.inscricaos = [];
-    vm.discplinas = [];
+
     Principal.identity().then(r => {
       var authorities = r.authorities;
       if(authorities.indexOf("ROLE_ADMIN") != -1 || authorities.indexOf('ROLE_SECRETARIA') != -1) {
         loadAll();
-      } else {
+      } else if(authorities.indexOf('ROLE_ALUNO')) {
         Inscricao.query(function(result) {
           vm.inscricaos = result.filter(function(element) {
-            return element.disciplina.professor.login === r.login;
+            console.log(element);
+            console.log(r);
+            return element.aluno.login === r.login;
           });
         });
       }
