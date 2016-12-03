20b94879ba43eb99b6d585eb2ead30c8a11235ee
viniciusscampos <silvcamposvinicius@gmail.com>
2016-12-03 18:06:36 -0200

    :new: Subscribe page student visualization restriction.

--- src/main/webapp/app/entities/inscricao/inscricao-dialog.controller.js
+++ src/main/webapp/app/entities/inscricao/inscricao-dialog.controller.js
@@ -5,18 +5,28 @@
         .module('jhipsterApp')
         .controller('InscricaoDialogController', InscricaoDialogController);
 
-    InscricaoDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Inscricao', 'Aluno', 'Disciplina'];
+    InscricaoDialogController.$inject = ['$timeout', '$scope', '$stateParams', 'Principal', '$uibModalInstance', 'entity', 'Inscricao', 'Aluno', 'Disciplina'];
 
-    function InscricaoDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Inscricao, Aluno, Disciplina) {
+    function InscricaoDialogController ($timeout, $scope, $stateParams, Principal, $uibModalInstance, entity, Inscricao, Aluno, Disciplina) {
         var vm = this;
 
         vm.inscricao = entity;
         vm.clear = clear;
         vm.datePickerOpenStatus = {};
         vm.openCalendar = openCalendar;
-        vm.save = save;
+        vm.save = save;        
+        vm.disciplinas = Disciplina.query();        
         vm.alunos = Aluno.query();
-        vm.disciplinas = Disciplina.query();
+
+        Principal.identity().then(r => {                        
+            var authorities = r.authorities;
+            if( authorities.indexOf("ROLE_ALUNO") != -1) {
+                $scope.alunos_names = [r.login];  
+            }            
+            else{                     
+                // ADICIONAR FUNCIONALIDADE QUE MODIFICA alunos_names                                  
+            }
+        });              
 
         $timeout(function (){
             angular.element('.form-group:eq(1)>input').focus();
